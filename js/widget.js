74e92cc98c5b41206e113406ac66c60e6a606529
Jakob Troidl <jakob.troidl@googlemail.com>
2024-03-26 17:07:00 -0400

    first minimal working version

--- js/widget.js
+++ js/widget.js
@@ -2,19 +2,20 @@ import "./widget.css";
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@6/+esm";
 
 function render({ model, el }) {
-  console.log("widget render");
+  let data = d3.csvParse(model.get("data"));
+
   // create a div for the first chart
   let button1 = document.createElement("button");
-  button1.innerHTML = `var1`;
+  button1.innerHTML = `outgoing_syn`;
   button1.addEventListener("click", () => {
-    update("var1");
+    update("outgoing_syn");
   });
   el.appendChild(button1);
 
   let button2 = document.createElement("button");
-  button2.innerHTML = `var2`;
+  button2.innerHTML = `incoming_syn`;
   button2.addEventListener("click", () => {
-    update("var2");
+    update("incoming_syn");
   });
   el.appendChild(button2);
 
@@ -25,8 +26,8 @@ function render({ model, el }) {
 
   // set the dimensions and margins of the graph
   const margin = { top: 30, right: 30, bottom: 70, left: 60 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    width = div.clientWidth - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
   const svg = d3
@@ -46,35 +47,30 @@ function render({ model, el }) {
   const yAxis = svg.append("g").attr("class", "myYaxis");
 
   // Initialize plot
-  update("var1");
+  update("outgoing_syn", data);
 
   // A function that create / update the plot for a given variable:
   function update(selectedVar) {
-    // Parse the Data
-    d3.csv(
-      "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/barplot_change_data.csv"
-    ).then(function (data) {
-      // X axis
-      x.domain(data.map((d) => d.group));
-      xAxis.transition().duration(1000).call(d3.axisBottom(x));
+    // X axis
+    x.domain(data.map((d) => d.cell_type));
+    xAxis.transition().duration(1000).call(d3.axisBottom(x));
 
-      // Add Y axis
-      y.domain([0, d3.max(data, (d) => +d[selectedVar])]);
-      yAxis.transition().duration(1000).call(d3.axisLeft(y));
+    // Add Y axis
+    y.domain([0, d3.max(data, (d) => +d[selectedVar])]);
+    yAxis.transition().duration(1000).call(d3.axisLeft(y));
 
-      // variable u: map data to existing bars
-      const u = svg.selectAll("rect").data(data);
+    // variable u: map data to existing bars
+    const u = svg.selectAll("rect").data(data);
 
-      // update bars
-      u.join("rect")
-        .transition()
-        .duration(1000)
-        .attr("x", (d) => x(d.group))
-        .attr("y", (d) => y(d[selectedVar]))
-        .attr("width", x.bandwidth())
-        .attr("height", (d) => height - y(d[selectedVar]))
-        .attr("fill", "#69b3a2");
-    });
+    // update bars
+    u.join("rect")
+      .transition()
+      .duration(1000)
+      .attr("x", (d) => x(d.cell_type))
+      .attr("y", (d) => y(d[selectedVar]))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => height - y(d[selectedVar]))
+      .attr("fill", "#69b3a2");
   }
 }
 
--- src/jbarchart/__init__.py
+++ src/jbarchart/__init__.py
@@ -1,5 +1,7 @@
 import importlib.metadata
 import pathlib
+from io import StringIO
+import ipywidgets as widgets
 
 import anywidget
 import traitlets
@@ -10,7 +12,34 @@ except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
 
+class BarChart:
+    def __init__(self, data):
+        self.widget = Counter()
+        self.update(data)
+
+    def update(self, data):
+        output = StringIO()
+        data.to_csv(output, index=False)
+        self.widget.data = output.getvalue()
+        output.close()
+
+    def show(self):
+        return self.widget
+
 class Counter(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
-    value = traitlets.Int(0).tag(sync=True)
+    data = traitlets.Unicode().tag(sync=True)
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    # def show(self):
+    #     # plots = widgets.VBox(
+    #     #     children=[self],
+    #     #     layout=widgets.Layout(
+    #     #         flex='1',
+    #     #         width='auto'
+    #     #     )
+    #     # )
+    #     return self
