d5d99ada045159189b86104ea673493bc46148ad
Jakob Troidl <jakob.troidl@googlemail.com>
2024-04-02 16:22:36 -0400

    minimal reproducer

---
+++ js/barchart.js
@@ -0,0 +1,65 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@6/+esm";
+
+export class BarChart {
+  constructor(parent, data, model) {
+    this.data = data;
+    this.parent = parent;
+    this.model = model;
+  }
+
+  init() {
+    // console.log('initialize the chart');
+
+    // set the dimensions and margins of the graph
+    const margin = { top: 30, right: 30, bottom: 70, left: 60 };
+    this.width = this.parent.clientWidth - margin.left - margin.right;
+    this.height = 500 - margin.top - margin.bottom;
+
+    let parentTag = "#" + this.parent.id;
+    this.svg = d3
+      .select(parentTag)
+      .append("svg")
+      .attr("width", this.width + margin.left + margin.right)
+      .attr("height", this.height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    this.x = d3.scaleBand().range([0, this.width]).padding(0.2);
+    this.xAxis = this.svg
+      .append("g")
+      .attr("transform", `translate(0,${this.height})`);
+
+    // Initialize the Y axis
+    this.y = d3.scaleLinear().range([this.height, 0]);
+    this.yAxis = this.svg.append("g").attr("class", "myYaxis");
+  }
+
+  update(variableName) {
+    this.x.domain(this.data.map((d) => d.cell_type));
+    this.xAxis.transition().duration(1000).call(d3.axisBottom(this.x));
+
+    // update the chart
+    this.y.domain([0, d3.max(this.data, (d) => +d[variableName])]);
+    this.yAxis.transition().duration(1000).call(d3.axisLeft(this.y));
+
+    const u = this.svg.selectAll("rect").data(this.data);
+
+    // update bars
+    u.join("rect")
+      .transition()
+      .duration(1000)
+      .attr("x", (d) => this.x(d.cell_type))
+      .attr("y", (d) => this.y(d[variableName]))
+      .attr("width", this.x.bandwidth())
+      .attr("height", (d) => this.height - this.y(d[variableName]))
+      .attr("fill", "#69b3a2");
+
+    d3.selectAll("rect")
+      .on("mouseover", function (d) {
+        d3.select(this).style("fill", "orange");
+      })
+      .on("mouseout", function (d) {
+        d3.select(this).style("fill", "#69b3a2"); // Assume 'steelblue' is the original color
+      });
+  }
+}
--- src/jbarchart/__init__.py
+++ src/jbarchart/__init__.py
@@ -1,7 +1,24 @@
+# import importlib.metadata
+# import pathlib
+
+# import anywidget
+# import traitlets
+
+# try:
+#     __version__ = importlib.metadata.version("jbarchart")
+# except importlib.metadata.PackageNotFoundError:
+#     __version__ = "unknown"
+
+
+# class Counter(anywidget.AnyWidget):
+#     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
+#     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
+#     value = traitlets.Int(0).tag(sync=True)
+
+
 import importlib.metadata
 import pathlib
 from io import StringIO
-import ipywidgets as widgets
 
 import anywidget
 import traitlets
@@ -11,35 +28,18 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
-
-class BarChart:
-    def __init__(self, data):
-        self.widget = Counter()
-        self.update(data)
-
-    def update(self, data):
-        output = StringIO()
-        data.to_csv(output, index=False)
-        self.widget.data = output.getvalue()
-        output.close()
-
-    def show(self):
-        return self.widget
-
-class Counter(anywidget.AnyWidget):
+class BarChart(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
     data = traitlets.Unicode().tag(sync=True)
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+    def update(self, data):
+        output = StringIO()
+        data.to_csv(output, index=False)
+        self.data = output.getvalue()
+        output.close()
+        return self
 
-    # def show(self):
-    #     # plots = widgets.VBox(
-    #     #     children=[self],
-    #     #     layout=widgets.Layout(
-    #     #         flex='1',
-    #     #         width='auto'
-    #     #     )
-    #     # )
-    #     return self
