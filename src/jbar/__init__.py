7a088a6cb340f6fa9e0363d5c222a66fa090c0e1
Jakob Troidl <jakob.troidl@googlemail.com>
2024-04-08 11:49:04 -0400

    add first version of dropdown

--- src/jbar/__init__.py
+++ src/jbar/__init__.py
@@ -4,6 +4,8 @@ from io import StringIO
 
 import anywidget
 import traitlets
+import ipywidgets as widgets
+
 
 try:
     __version__ = importlib.metadata.version("jbar")
@@ -14,14 +16,45 @@ class BarChart(anywidget.AnyWidget):
     _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
     _css = pathlib.Path(__file__).parent / "static" / "widget.css"
     data = traitlets.Unicode().tag(sync=True)
+    x = traitlets.Unicode().tag(sync=True)
+    exclude = traitlets.List().tag(sync=True)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         
-    def update(self, data):
+    def update(self, data, x, exclude=[]):
+        self.x = x
+        self.exclude = exclude
+
+        # remove unwanted columns
+        if self.exclude:
+            data.drop(self.exclude, axis=1, inplace=True)
+        
         output = StringIO()
         data.to_csv(output, index=False)
         self.data = output.getvalue()
         output.close()
-        return self
+        
+    
+    def dropdown_change(self, change):
+        print(change)
+        
+    def show(self):
+        dropdown = widgets.Dropdown(
+            options=[('One', 1), ('Two', 2), ('Three', 3)],
+            value=2,
+            description='Number:',
+        )
+
+        dropdown.observe(self.dropdown_change)
+
+        plot = widgets.VBox(
+            children=[self],
+            layout=widgets.Layout(
+                flex='1',
+                width='auto'
+            )
+        )
+        return widgets.VBox([dropdown, plot])
+        
 
